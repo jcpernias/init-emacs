5ae0d9f9f33b9eafca62fdb37e8ec085d9257c55
José C. Pernías <jcpernias@gmail.com>
2019-08-16 00:03:50 +0200

    Install use-package

--- init.el
+++ init.el
@@ -1,7 +1,39 @@
-;; Use right option key to type \, |, @, and # characters
-(setq ns-right-alternate-modifier (quote none))
-
 ;; Disable the scrollbar, the toolbar and the menu bar
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+
+;; Do not clutter init.el with customize settings:
+(setq custom-file
+      (expand-file-name (concat user-emacs-directory "custom.el")))
+(if (file-exists-p custom-file)
+    (load custom-file))
+
+
+;; set up the Emacs package system
+;; (see http://emacs.stackexchange.com/a/5888) 
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;; package repositories
+(setq package-archives
+      '(("gnu"         . "https://elpa.gnu.org/packages/")
+	("melpa"       . "https://melpa.org/packages/")
+	("org"         . "http://orgmode.org/elpa/")))
+
+;; Initialize Emacs package system:
+(unless package--initialized
+  (package-initialize))
+
+
+;; Install use-package (https://github.com/jwiegley/use-package).
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+
+;; macOS: Use right option key to type \, |, @, and # characters
+(setq ns-right-alternate-modifier (quote none))
+
