8ad76effeba76d54cc5d9f2d73b479974230f759
José C. Pernías <jcpernias@gmail.com>
2019-08-16 12:47:45 +0200

    Disable horizontal scrollbars

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'horizontal-scroll-mode) (horizontal-scroll-mode -1))
 
 ;; Do not show start up screen:
 (setq inhibit-startup-screen t)
