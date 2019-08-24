4f97a55615dac7965bd1d482e392831282b7ac9f
José C. Pernías <jcpernias@gmail.com>
2019-08-24 13:26:39 +0200

    Use C-w for kill the current word

--- init.el
+++ init.el
@@ -41,6 +41,14 @@
 (global-set-key (kbd "s-SPC") 'just-one-space)
 
 
+;; C-w kill word backwards when no region is set
+(defadvice kill-region (before unix-werase activate compile)
+  "When called interactively with no active region, delete a single word
+    backwards instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (save-excursion (backward-word 1) (point)) (point)))))
+
 ;; Align your code in a pretty way.
 (global-set-key (kbd "C-x \\") 'align-regexp) 
 
@@ -481,5 +489,3 @@
 (use-package restart-emacs
   :ensure t
   :bind ("C-c C-z" . restart-emacs))
-
-
