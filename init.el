bb166aa89ac8a914dc15e211fac5cdada63b5a3c
José C. Pernías <jcpernias@gmail.com>
2022-03-03 21:52:53 +0100

    Load the macOS specific code before other cofiguration files

--- init.el
+++ init.el
@@ -2,11 +2,11 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "jcp-init"))
 
+;; mac key bindings
+(when (eq system-type 'darwin)
+  (require 'jcp-darwin))
+
 (require 'jcp-base)
 (require 'jcp-extensions)
 (require 'jcp-functions)
 (require 'jcp-global-keys)
-
-;; mac key bindings
-(when (eq system-type 'darwin)
-  (require 'jcp-darwin))
