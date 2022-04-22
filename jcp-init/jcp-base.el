50bfb2586c60f52270e26c536844f908e98397ec
José C. Pernías <jcpernias@gmail.com>
2022-04-22 12:34:41 +0200

    Format code

--- jcp-init/jcp-base.el
+++ jcp-init/jcp-base.el
@@ -190,12 +190,13 @@
 (setq compilation-scroll-output t)
 
 ;; hippie expand
-(setq hippie-expand-try-functions-list '(try-expand-dabbrev
-      try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill
-      try-complete-file-name-partially try-complete-file-name
-      try-expand-all-abbrevs try-expand-list try-expand-line
-      try-complete-lisp-symbol-partially
-      try-complete-lisp-symbol))
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill
+        try-complete-file-name-partially try-complete-file-name
+        try-expand-all-abbrevs try-expand-list try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 ;; apropos
 (setq apropos-do-all t)
