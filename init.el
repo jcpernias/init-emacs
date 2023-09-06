498e04a6365b6e93cce67f2a0c48dee97f7d8868
José C. Pernías <jcpernias@gmail.com>
2023-09-06 15:36:34 +0200

    Adjust garbage collection thresholds

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
-;;; Main init file
+;;; Main init file -*- lexical-binding: t -*-
 
 (add-to-list 'load-path (concat user-emacs-directory "jcp-init"))
+
+;; Adjust garbage collection thresholds during startup and thereafter
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 (require 'jcp-functions)
 
@@ -11,3 +18,5 @@
 (require 'jcp-base)
 (require 'jcp-extensions)
 (require 'jcp-global-keys)
+
+(provide 'init)
